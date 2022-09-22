8a68f83c51a0416f00c6935e63f3535eb32f5b0c
Vitorir <vitorireboucas@edu.unifor.br>
2022-09-22 14:46:20 -0300

    Changes

---
+++ src/Cliente.java
@@ -0,0 +1,12 @@
+public class Cliente {
+    private String CPF;
+    private String Nome;
+    private String Telefone;
+    private String Endereco;
+    private String msgPromo;
+
+    public int Comprar() {
+
+        return 0;
+    }
+}
--- src/Loja.java
+++ src/Loja.java
@@ -2,4 +2,9 @@ public class Loja {
     private Fornecedor fornecedor;
     private Produto produto;
 
+    private static int comprar() {
+
+        return 0;
+    }
+
 }
