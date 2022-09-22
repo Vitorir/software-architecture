83f78272407064905088d2bf7fe405a8c5956b1e
Vitorir <vitorireboucas@edu.unifor.br>
2022-09-22 16:37:53 -0300

    Changes Fornecedor

--- src/Fornecedor.java
+++ src/Fornecedor.java
@@ -6,4 +6,48 @@ public class Fornecedor {
     private String email;
     private String endereco;
 
+    public Fornecedor(Produto produto, String nome) {
+        this.produto = produto;
+        this.nome = nome;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
 }
